9ea893029e6232ec32607164fa80b478b4378c2e
Andrew Dawson <dawso.andrew@gmail.com>
2018-11-30 21:57:42 -0500

    Simplifies things and adds a couple comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
 "use strict";
 
-function lerp(v0, v1, t) {
-  return ((1 - t) * v0) + (t * v1);
-}
-
-function randomIntRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+let crystalValues = [];
+let goalNumber = 0;
+let losses = 0;
+let score = 0;
+let wins = 0;
 
 
 class Colour {
@@ -17,87 +15,89 @@ class Colour {
   }
 
   makeRgbText() {
-    const r = Math.floor(255 * this.r);
-    const g = Math.floor(255 * this.g);
-    const b = Math.floor(255 * this.b);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+    return "rgb(" + this.r + ", " + this.g + ", " + this.b + ")";
   }
 
-  static lerp(v0, v1, t) {
-    const r = lerp(v0.r, v1.r, t);
-    const g = lerp(v0.g, v1.g, t);
-    const b = lerp(v0.b, v1.b, t);
+  // Compute a colour some fraction of the way between two colours by linearly
+  // interpolating their components.
+  static blend(v0, v1, t) {
+    const r = Math.floor(lerp(v0.r, v1.r, t));
+    const g = Math.floor(lerp(v0.g, v1.g, t));
+    const b = Math.floor(lerp(v0.b, v1.b, t));
     return new Colour(r, g, b);
   }
 }
 
 
-class Game {
-  constructor() {
-    this.crystalValues = [];
-    this.goalNumber = 0;
-    this.losses = 0;
-    this.score = 0;
-    this.wins = 0;
+function animateScoreColour() {
+  const mix = score / goalNumber;
+  const endColour = new Colour(255, 15, 59);
+  const startColour = new Colour(175, 175, 175);
+  const colour = Colour.blend(startColour, endColour, mix);
+  const value = colour.makeRgbText();
+  $(".score-group").css("background-color", value);
+}
 
-    $("#losses").text("0");
-    $("#wins").text("0");
-  }
+function chooseCrystal(index) {
+  score += crystalValues[index];
 
-  animateScoreColour() {
-    const mix = this.score / this.goalNumber;
-    const endColour = new Colour(1, .0588, .2313);
-    const startColour = new Colour(.6863, .6863, .6863);
-    const colour = Colour.lerp(startColour, endColour, mix);
-    const value = colour.makeRgbText();
-    $(".score-group").css("background-color", value);
-  }
+  $("#score").text(score);
 
-  chooseCrystal(index) {
-    this.score += this.crystalValues[index];
-    $("#score").text(this.score);
-
-    if (this.score > this.goalNumber) {
-      this.lose();
-    } else if (this.score == this.goalNumber) {
-      this.win();
-    } else {
-      this.animateScoreColour();
-    }
-  }
-
-  lose() {
-    this.losses += 1;
-    $("#losses").text(this.losses);
-    this.startNewGame();
-  }
-
-  startNewGame() {
-    for (let i = 0; i < 4; i++) {
-      this.crystalValues[i] = randomIntRange(1, 12);
-    }
-    this.goalNumber = randomIntRange(19, 120);
-    this.score = 0;
-    $("#goal-number").text(this.goalNumber);
-    $("#score").text(this.score);
-    $(".score-group").css("background-color", "#afafaf");
-  }
-
-  win() {
-    this.wins += 1;
-    $("#wins").text(this.wins);
-    this.startNewGame();
+  if (score > goalNumber) {
+    lose();
+  } else if (score == goalNumber) {
+    win();
+  } else {
+    animateScoreColour();
   }
 }
 
+// Compute a number some fraction of the way between two numbers (linear
+// interpolation). t is the fraction from 0 to 1, and v0 and v1 are the numbers.
+function lerp(v0, v1, t) {
+  return ((1 - t) * v0) + (t * v1);
+}
 
-$(document).ready(() => {
-  const game = new Game();
-  game.startNewGame();
+function lose() {
+  losses++;
+  $("#losses").text(losses);
+  startNewGame();
+}
 
-  $(".crystal").click((event) => {
-    const value = $(event.currentTarget).val();
+function randomIntRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function startNewGame() {
+  for (let i = 0; i < 4; i++) {
+    crystalValues[i] = randomIntRange(1, 12);
+  }
+  goalNumber = randomIntRange(19, 120);
+  score = 0;
+  $("#goal-number").text(goalNumber);
+  $("#score").text(score);
+  $(".score-group").css("background-color", "#afafaf");
+}
+
+function win() {
+  wins++;
+  $("#wins").text(wins);
+  startNewGame();
+}
+
+function zeroStats() {
+  $("#losses").text("0");
+  $("#wins").text("0");
+}
+
+
+$(document).ready(function() {
+  zeroStats();
+  startNewGame();
+
+  $(".crystal").click(function() {
+    const value = $(this).val();
     const index = parseInt(value);
-    game.chooseCrystal(index);
+    chooseCrystal(index);
   });
 });
